f6280cc8df1bcd58c0e454fe51ee865ee7f79e1a
ybn4aq <ybn4aq@virginia.edu>
2023-06-06 19:56:35 -0400

    Added bootstrap

--- gameHub/src/App.jsx
+++ gameHub/src/App.jsx
@@ -1,10 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
+import 'bootstrap/dist/css/bootstrap.css';
+
 
 function App() {
-  
+  <div>
+    <button>
+      Helen moment
+    </button>
+  </div>
 }
 
 export default App

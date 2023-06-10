a58b5385c6fb3a7bbc89b4ee4a005894d9dd6b3f
larissacybyk <jdj9ng@virginia.edu>
2023-06-09 21:35:23 -0400

    formatting!

--- gameHub/src/components/navbar.component.jsx
+++ gameHub/src/components/navbar.component.jsx
@@ -5,17 +5,17 @@ export default class NavBar extends Component {
     render() {
         return (
             <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand">GameHub</Link>
+                <Link to="/" className="navbar-brand ms-2">GameHub</Link>
                 <div>   
                     <ul className="navbar-nav mr-auto d-flex flex-row">
                         <li className="navbar-item">
                             <Link to="/" className="nav-link">High Scores</Link>
                         </li>
-                        <li className="navbar-item  ms-2">
+                        <li className="navbar-item  ms-2 " >
                             <Link to="/create" className="nav-link">Create Score</Link>
                         </li>
                         <li className="navbar-item  ms-2">
-                            <Link to="/play" className="nav-link">Play Game</Link>
+                            <Link to="/play" className="nav-link me-2">Play Game</Link>
                         </li>
                     </ul>
                 </div>

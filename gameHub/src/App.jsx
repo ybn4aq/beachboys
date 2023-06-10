d5094c589084393e0df245c40e63cabaf60d214c
larissacybyk <jdj9ng@virginia.edu>
2023-06-09 21:35:10 -0400

    Formatting

--- gameHub/src/App.jsx
+++ gameHub/src/App.jsx
@@ -14,13 +14,17 @@ function App() {
         style={{
           position: "absolute",
           top: "0%",
+          width: "100%",
           left: "0%",
+          whitespace:"nowrap"
         }}
       >
-        <Router>
+        <Router >
           <NavBar />
-          
-          <Routes>
+          <Routes style={{
+            left: "0%",
+            whitespace:"nowrap"
+          }}>
             <Route path="/" exact element={<HighScoresList />} />
             <Route path="/create" element={<CreateScore />} />
             <Route path="/play" element={<PlayGame />} />

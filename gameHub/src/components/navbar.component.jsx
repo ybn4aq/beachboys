8d9e6463123eb16115204268ed8b3ef5465e001b
ybn4aq <ybn4aq@virginia.edu>
2023-06-12 21:33:58 -0400

    made home page

--- gameHub/src/components/navbar.component.jsx
+++ gameHub/src/components/navbar.component.jsx
@@ -1,25 +1,33 @@
-import React, { Component } from 'react';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
-    render() {
-        return (
-            <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
-                <Link to="/" className="navbar-brand ms-2">GameHub</Link>
-                <div>   
-                    <ul className="navbar-nav mr-auto d-flex flex-row">
-                        <li className="navbar-item">
-                            <Link to="/" className="nav-link">High Scores</Link>
-                        </li>
-                        <li className="navbar-item  ms-2 " >
-                            <Link to="/create" className="nav-link">Create Score</Link>
-                        </li>
-                        <li className="navbar-item  ms-2">
-                            <Link to="/play" className="nav-link me-2">Play Game</Link>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-        )
-    }
+  render() {
+    return (
+      <nav className="navbar navbar-dark bg-dark navbar-expand-lg">
+        <Link to="/" className="navbar-brand ms-2">
+          GameHub
+        </Link>
+        <div>
+          <ul className="navbar-nav mr-auto d-flex flex-row">
+            <li className="navbar-item">
+              <Link to="/highscores" className="nav-link">
+                High Scores
+              </Link>
+            </li>
+            <li className="navbar-item  ms-2 ">
+              <Link to="/create" className="nav-link">
+                Create Score
+              </Link>
+            </li>
+            <li className="navbar-item  ms-2">
+              <Link to="/play" className="nav-link me-2">
+                Play Game
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }

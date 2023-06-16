0ab844360f336067bf97b62a868619da72704ff6
HELEN YAO <helenroseyao@gmail.com>
2023-06-15 20:40:20 -0500

    bootstrap script in index file and started highscores

--- gameHub/src/components/pages/highscores-list.page.jsx
+++ gameHub/src/components/pages/highscores-list.page.jsx
@@ -3,9 +3,38 @@ import { Link } from "react-router-dom";
 
 export default class HighScoresList extends Component {
   render() {
+    const scores = [
+      { username: "User1", game: "Pac Man", score: 13 },
+      { username: "User2", game: "Pac Man", score: 24 },
+    ];
+    const mappedArray = scores.map((item) => <div>{JSON.stringify(item)}</div>);
+
     return (
-      <div>
-        <p>ur on high scores list component</p>
+      <div class="hstack gap-3">
+        <h1>High Scores</h1>
+        <div class="dropdown">
+          <button
+            class="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="true"
+          >
+            Filter by
+          </button>
+          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+            <a class="dropdown-item" href="#">
+              Game
+            </a>
+            <a class="dropdown-item" href="#">
+              User
+            </a>
+            {/* <a class="dropdown-item" href="javascript:itemSelected('A')">
+              A. Hello World!
+            </a> */}
+          </div>
+        </div>
       </div>
     );
   }

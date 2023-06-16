336814969293c4098a2215f8ae66fe7d758adf0a
HELEN YAO <helenroseyao@gmail.com>
2023-06-15 21:58:17 -0500

    added changes to App.css and started highscores

--- gameHub/src/components/pages/highscores-list.page.jsx
+++ gameHub/src/components/pages/highscores-list.page.jsx
@@ -1,38 +1,67 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
 
 export default class HighScoresList extends Component {
   render() {
-    const scores = [
+    const data = [
       { username: "User1", game: "Pac Man", score: 13 },
       { username: "User2", game: "Pac Man", score: 24 },
+      { username: "User2", game: "Tic Tac Toe", score: 24 },
+      { username: "User2", game: "Battleship", score: 24 },
+      { username: "User3", game: "Pac Man", score: 24 },
+      { username: "User4", game: "Pac Man", score: 24 },
+      { username: "User4", game: "Battleship", score: 24 },
+      { username: "User5", game: "Mario Kart", score: 24 },
+      { username: "User6", game: "Mario Kart", score: 24 },
     ];
-    const mappedArray = scores.map((item) => <div>{JSON.stringify(item)}</div>);
+
+    const usernames = data.map((item, index) => (
+      <div key={index}>{item.username}</div>
+    ));
+    const scores = data.map((item, index) => (
+      <div key={index}>{item.score}</div>
+    ));
+    const games = data.map((item, index) => <div key={index}>{item.game}</div>);
 
     return (
-      <div class="hstack gap-3">
-        <h1>High Scores</h1>
-        <div class="dropdown">
-          <button
-            class="btn btn-secondary dropdown-toggle"
-            type="button"
-            id="dropdownMenuButton"
-            data-bs-toggle="dropdown"
-            aria-haspopup="true"
-            aria-expanded="true"
-          >
-            Filter by
-          </button>
-          <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
-            <a class="dropdown-item" href="#">
-              Game
-            </a>
-            <a class="dropdown-item" href="#">
-              User
-            </a>
-            {/* <a class="dropdown-item" href="javascript:itemSelected('A')">
-              A. Hello World!
-            </a> */}
+      <div>
+        <div className="rowContainer">
+          <h1>High Scores</h1>
+          <div className="dropdown">
+            <button
+              className="btn btn-secondary dropdown-toggle"
+              type="button"
+              id="dropdownMenuButton"
+              data-bs-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="true"
+            >
+              Filter by
+            </button>
+            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
+              <h7 className="dropdown-header" type="button">
+                Game
+              </h7>
+              <button className="dropdown-item" type="button">
+                Tic Tac Toe
+              </button>
+              <button className="dropdown-item" type="button">
+                User
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className="rowContainer" style={{ marginTop: 20 }}>
+          <div className="columnContainer">
+            <h3>Users</h3>
+            {usernames}
+          </div>
+          <div className="columnContainer">
+            <h3>Scores</h3>
+            {scores}
+          </div>
+          <div className="columnContainer">
+            <h3>Games</h3>
+            {games}
           </div>
         </div>
       </div>

be90fb591d3f12957786995bb021946c1680693c
HELEN YAO <helenroseyao@gmail.com>
2023-06-17 16:28:54 -0500

    broke functionality messed with class at the top and changed to function

---
+++ gameHub/src/components/HighScoresData.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const scoreData = [
+  { username: "User1", game: "Pac Man", score: 13 },
+  { username: "User2", game: "Pac Man", score: 24 },
+  { username: "User2", game: "Tic Tac Toe", score: 24 },
+  { username: "User2", game: "Battleship", score: 24 },
+  { username: "User3", game: "Pac Man", score: 24 },
+  { username: "User4", game: "Pac Man", score: 24 },
+  { username: "User4", game: "Battleship", score: 24 },
+  { username: "User5", game: "Mario Kart", score: 24 },
+  { username: "User6", game: "Mario Kart", score: 24 },
+];
--- gameHub/src/components/pages/highscores-list.page.jsx
+++ gameHub/src/components/pages/highscores-list.page.jsx
@@ -1,77 +1,106 @@
-import React, { Component } from "react";
-import Select from "react-select";
+import React, { Component, useState } from "react";
+import { scoreData } from "../HighScoresData";
 
-export default class HighScoresList extends Component {
-  render() {
-    const data = [
-      { username: "User1", game: "Pac Man", score: 13 },
-      { username: "User2", game: "Pac Man", score: 24 },
-      { username: "User2", game: "Tic Tac Toe", score: 24 },
-      { username: "User2", game: "Battleship", score: 24 },
-      { username: "User3", game: "Pac Man", score: 24 },
-      { username: "User4", game: "Pac Man", score: 24 },
-      { username: "User4", game: "Battleship", score: 24 },
-      { username: "User5", game: "Mario Kart", score: 24 },
-      { username: "User6", game: "Mario Kart", score: 24 },
-    ];
+export function HighScoresList() {
+  const filterOptions = [
+    { value: "ticTacToe", label: "Tic Tac Toe" },
+    { value: "battleship", label: "Battleship" },
+    { value: "marioKart", label: "Mario Kart" },
+  ];
 
-    const filterOptions = [
-      { value: "ticTacToe", label: "Tic Tac Toe" },
-      { value: "battleship", label: "Battleship" },
-      { value: "marioKart", label: "Mario Kart" },
-    ];
+  const [filterValue, setFilterValue] = useState("");
+  const handleFilter = (event) => {
+    console.log(event);
+    setFilterValue(event);
+  };
 
-    const usernames = data.map((item, index) => (
-      <div key={index}>{item.username}</div>
-    ));
-    const scores = data.map((item, index) => (
-      <div key={index}>{item.score}</div>
-    ));
-    const games = data.map((item, index) => <div key={index}>{item.game}</div>);
+  const usernames = scoreData.map((item, index) => (
+    <div key={index}>{item.username}</div>
+  ));
+  const scores = scoreData.map((item, index) => (
+    <div key={index}>{item.score}</div>
+  ));
+  const games = scoreData.map((item, index) => (
+    <div key={index}>{item.game}</div>
+  ));
 
-    return (
-      <div>
-        <div className="rowContainer">
-          <h1>High Scores</h1>
-          <div className="dropdown">
-            <button
-              className="btn btn-secondary dropdown-toggle"
-              type="button"
-              id="dropdownMenuButton"
-              data-bs-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="true"
-            >
-              Filter by
+  // const handleFilter = (event) => {
+  //   console.log("handle filter");
+  // };
+
+  return (
+    <div>
+      <div className="rowContainer">
+        <h1>High Scores</h1>
+        <div className="dropdown">
+          <button
+            className="btn btn-secondary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton"
+            data-bs-toggle="dropdown"
+            aria-haspopup="true"
+            aria-expanded="true"
+          >
+            Filter by
+          </button>
+          <div
+            className="dropdown-menu"
+            aria-labelledby="dropdownMenuButton"
+            onChange={console.log("test")}
+          >
+            <h7 className="dropdown-header" type="button">
+              Game
+            </h7>
+            <button className="dropdown-item" type="button">
+              Tic Tac Toe
             </button>
-            <div className="dropdown-menu" aria-labelledby="dropdownMenuButton">
-              <h7 className="dropdown-header" type="button">
-                Game
-              </h7>
-              <button className="dropdown-item" type="button">
-                Tic Tac Toe
-              </button>
-              <button className="dropdown-item" type="button">
-                User
+            <h7 className="dropdown-header" type="button">
+              Score
+            </h7>
+            <button className="dropdown-item" type="button">
+              High-to-low
+            </button>
+            <button className="dropdown-item" type="button">
+              Low-to-high
+            </button>
+            <h7 className="dropdown-header" type="button">
+              User
+            </h7>
+            <div class="input-group input-group-sm mb-3">
+              <span class="input-group-text" id="inputGroup-sizing-sm">
+                username:
+              </span>
+              <input
+                type="text"
+                class="form-control"
+                aria-label="enter-username"
+                aria-describedby="inputGroup-sizing-sm"
+              />
+              <button
+                class="btn btn-outline-secondary btn-sm"
+                type="button"
+                id="button-addon2"
+              >
+                Button
               </button>
             </div>
           </div>
         </div>
-        <div className="rowContainer" style={{ marginTop: 20 }}>
-          <div className="columnContainer">
-            <h3>Users</h3>
-            {usernames}
-          </div>
-          <div className="columnContainer">
-            <h3>Scores</h3>
-            {scores}
-          </div>
-          <div className="columnContainer">
-            <h3>Games</h3>
-            {games}
-          </div>
+      </div>
+      <div className="rowContainer" style={{ marginTop: 20 }}>
+        <div className="columnContainer">
+          <h3>Users</h3>
+          {usernames}
+        </div>
+        <div className="columnContainer">
+          <h3>Scores</h3>
+          {scores}
+        </div>
+        <div className="columnContainer">
+          <h3>Games</h3>
+          {games}
         </div>
       </div>
-    );
-  }
+    </div>
+  );
 }

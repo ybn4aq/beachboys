ef1e6d59ef51ab41f03ba96c4e1b3bb2965e5549
ybn4aq <ybn4aq@virginia.edu>
2023-06-26 16:15:00 -0400

    progress

---
+++ gameHub/src/components/games/click-game.jsx
@@ -0,0 +1,10 @@
+import { useState } from 'react';
+import React, {Component} from 'react';
+
+class Clickgame extends Component {
+    render() {
+        return (
+            
+        )
+    }
+}
